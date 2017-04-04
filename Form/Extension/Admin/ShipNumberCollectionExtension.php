fdff5b8ca6c2aac79104ce3674d7bc0b261ef8d2
Yukihisa Tago <yukihisa.tago@gmail.com>
2017-04-04 14:31:48 +0900

    ShipNumberプラグイン3.0.14対応
    管理画面の受注管理で配送伝票番号欄への入力を任意とする

--- Form/Extension/Admin/ShipNumberCollectionExtension.php
+++ Form/Extension/Admin/ShipNumberCollectionExtension.php
@@ -3,7 +3,7 @@
 * This file is part of EC-CUBE
 *
 * Copyright(c) 2015 Takashi Otaki All Rights Reserved.
-* 
+*
 *
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
@@ -24,6 +24,7 @@ class ShipNumberCollectionExtension extends AbstractTypeExtension
             ->add('content', 'text', array(
                 'label' => '配送伝票番号',
                 'mapped' => false,
+                'required' => false,
             ))
       ;
     }
